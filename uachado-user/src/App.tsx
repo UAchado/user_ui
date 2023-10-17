6da1eaf1ca42f174089a3d7a4b0085768fdf277b
Daniel <danielmartinsferreira@ua.pt>
2023-10-17 17:33:12 +0100

    responsivity

--- uachado-user/src/App.tsx
+++ uachado-user/src/App.tsx
@@ -16,16 +16,16 @@ function App() {
           </button>
         </div>
       </div>
-      <div className='flex justify-center items-center h-screen '>
+      <div className=' flex justify-center items-center h-screen '>
         <div className="mockup-window bg-primary-focus">
-          <div className="flex justify-center px-20 py-16 bg-primary gap-20">
-            <div className="card w-96 image-full hover:cursor-pointer ">
-              <figure className='shadow-xl '><img src={placesLogo} alt="Places Icon" /></figure>
+          <div className="sm:flex flex-column justify-center px-20 py-16 bg-primary gap-40">
+            <div className="card w-96 image-full hover:cursor-pointer mb-10 py-10">
+              <figure className='shadow-xl'><img src={placesLogo} alt="Places Icon" /></figure>
               <div className="card-body m-auto">
                 <h2 className="card-title text-5xl">Find Authorized Points</h2>
               </div>
             </div>
-            <div className="card w-96 image-full hover:cursor-pointer">
+            <div className="card w-96 image-full hover:cursor-pointer mb-10 py-10">
               <figure className='shadow-xl'><img src={itemsLogo} alt="Places Icon" /></figure>
               <div className="card-body m-auto">
                 <h2 className="card-title text-5xl">Find Lost Items</h2>
