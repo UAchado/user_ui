3f216a7c4c53dcaff71e67f0adbf29a309d74504
Daniel <danielmartinsferreira@ua.pt>
2023-10-22 23:49:36 +0100

    added original cards

--- uachado-user/src/components/Home/home.tsx
+++ uachado-user/src/components/Home/home.tsx
@@ -6,24 +6,24 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div className="sm:flex flex-column justify-center px-20 py-16 bg-primary gap-40">
+      <div className="justify-center gap-40 px-20 py-16 sm:flex flex-column bg-primary">
         <Link to="/dropPoints">
-          <div className="card custom-card w-96 image-full hover:cursor-pointer mb-10 py-10">
+          <div className="w-full py-10 mb-10 card custom-card image-full hover:cursor-pointer">
             <figure className="shadow-xl">
               <img src={placesLogo} alt="Places Icon" />
             </figure>
-            <div className="card-body m-auto">
-              <h2 className="card-title text-5xl">Find Authorized Points</h2>
+            <div className="m-auto card-body">
+              <h2 className="text-5xl card-title">Pontos Autorizados</h2>
             </div>
           </div>
         </Link>
         <Link to="/findItems">
-          <div className="card custom-card w-96 image-full hover:cursor-pointer mb-10 py-10">
+          <div className="w-full py-10 mb-10 card custom-card image-full hover:cursor-pointer">
             <figure className="shadow-xl">
               <img src={itemsLogo} alt="Places Icon" />
             </figure>
-            <div className="card-body m-auto">
-              <h2 className="card-title text-5xl">Find Lost Items</h2>
+            <div className="m-auto card-body">
+              <h2 className="text-5xl card-title">Objetos Perdidos</h2>
             </div>
           </div>
         </Link>
