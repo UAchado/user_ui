4990c9dfab3f02c6245287c3ac526773cda42217
Pjnp5 <paulojnpinto02@ua.pt>
2023-10-23 18:04:45 +0100

    New item changes

--- uachado-user/src/components/NewItem/newItem.tsx
+++ uachado-user/src/components/NewItem/newItem.tsx
@@ -51,25 +51,18 @@ const NewItem = () => {
     const formData = new FormData(e.currentTarget);
 
     // Access form fields directly from formData
-    const description = formData.get("description");
     const image = formData.get("image") as File;
     const tag = formData.get("tag");
 
-    // Now, formData contains the form data, and you can decide how to process it.
-    console.log("Description:", description);
-    console.log("Image:", image);
-    console.log("Tag:", tag);
-
     if (tag != null) {
       if (tags.includes(tag.toString())) {
-
         const reader = new FileReader();
         reader.readAsDataURL(image);
         reader.onload = function () {
           const base64Image = reader.result as string;
           formData.set("image", base64Image);
           axios
-            .post("https://eo9s62lz76myhtf.m.pipedream.net", formData)
+            .post("https://eo9s62lz76myhtf.m.pipedream.net", formData) // change to post request to the server
             .then(function (response) {
               console.log("Data sent successfully:", response.data);
               setSucess("Tag is valid");
@@ -85,9 +78,49 @@ const NewItem = () => {
   };
 
   return (
-    <div className="toast toast-middle toast-center">
+    <div className="toast toast-middle toast-center items-center">
+      {warning && (
+        <div className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            onClick={() => setWarning(null)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span style={{ fontSize: "11px" }}>
+            Input a tag that exists on the list.
+          </span>
+        </div>
+      )}
+      {sucess && (
+        <div className="alert alert-success w-1/2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            onClick={() => setSucess(null)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span style={{ fontSize: "11px" }}>Sucessfully added item</span>
+        </div>
+      )}
       <div className="flex justify-center">
-        <div className="card bg-primary mt-20">
+        <div className="card bg-primary">
           <div className="card-body">
             <h2 className="card-title">Formulário de novo Objeto Perdido</h2>
             <form onSubmit={handleSubmit}>
@@ -118,44 +151,6 @@ const NewItem = () => {
           </div>
         </div>
       </div>
-      {warning && (
-        <div className="alert alert-error">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            onClick={() => setWarning(null)}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>input a tag that exists on the list.</span>
-        </div>
-      )}
-      {sucess && (
-        <div className="alert alert-sucess">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            onClick={() => setWarning(null)}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>Sucessfully added item</span>
-        </div>
-      )}
     </div>
   );
 };
