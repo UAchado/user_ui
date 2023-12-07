f58bbc7f6acb84c76fbc2387c2e03a7d3566a62b
Daniel <danielmartinsferreira@ua.pt>
2023-12-07 23:15:06 +0000

    Refactor item list to adjust UI based on screen width
    Removed unused useStates and commented out code in the ItemListPage. Integrated responsive design by adding a useState and useEffect to track window width, adjusting the component display based on a specified breakpoint. This provides a more dynamic UI, rendering a table for wider screens and a placeholder 'Hello World' for smaller ones.

--- uachado-user/src/pages/ItemListPage/itemlist.tsx
+++ uachado-user/src/pages/ItemListPage/itemlist.tsx
@@ -6,18 +6,23 @@ import axios from "axios";
 import { useJsApiLoader } from "@react-google-maps/api";
 
 const ItemList = () => {
+    interface DropPoint {
+        id: number;
+        name: string;
+        coordinates: string;
+        latitude: number;
+        longitude: number;
+    }
   const itemsBaseUrl = import.meta.env.VITE_INVENTORY_URL;
   const pointsBaseUrl = import.meta.env.VITE_POINTS_URL;
   const [tags, setTags] = useState<string[]>([]);
-  const [dropPoints, setDropoints] = useState<any[]>([]);
-  //const [locationData, setLocationData] = useState(null); // State to hold the location data
+  const [dropPoints, setDropoints] = useState<DropPoint[]>([]);
   const [selectedTag, setSelectedTag] = useState<string>("Todos");
   const [showMap, setShowMap] = useState(false);
   const [userLocation, setUserLocation] = useState<{
     lat: number;
     lng: number;
   } | null>(null);
-
   const data = [
     {
       image:
@@ -62,7 +67,19 @@ const ItemList = () => {
       dropoffPoint_id: 2,
     },
   ];
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
 
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   const [selectedItem, setSelectedItem] = useState<{
     image: string;
     description: string;
@@ -72,7 +89,7 @@ const ItemList = () => {
 
   useEffect(() => {
     if (selectedItem !== null) {
-      const modal: any = document.getElementById("my_modal_1");
+      const modal = document.getElementById("my_modal_1") as HTMLDialogElement;
       modal.showModal();
     }
   }, [selectedItem]);
@@ -138,26 +155,10 @@ const ItemList = () => {
         console.error("Error fetching dropPoints:", error);
       }
     };
-    fetchTags();
-    fetchDropPoints();
-  }, []);
+      fetchTags().then(() => console.log("Tags fetched successfully"));
+      fetchDropPoints().then(() => console.log("DropPoints fetched successfully"));
+  }, [itemsBaseUrl, pointsBaseUrl]);
 
-  // useEffect(() => {
-  //   // Function to fetch the location data when selectedItem changes
-  //   async function fetchLocationData() {
-  //     try {
-  //       const data = await getDropOffPoint(selectedItem?.dropoffPoint_id);
-  //       setLocationData(data);
-  //     } catch (error) {
-  //       // Handle any errors that may occur during the fetch
-  //       console.error('Error fetching location data:', error);
-  //     }
-  //   }
-
-  //   if (showMap && selectedItem) {
-  //     fetchLocationData(); // Fetch location data when showMap and selectedItem are truthy
-  //   }
-  // }, [showMap, selectedItem]);
 
   const calculateMidpoint = (
     lat1: number,
@@ -165,15 +166,15 @@ const ItemList = () => {
     lat2: number,
     lng2: number
   ): { lat: number; lng: number } | null => {
-    let dLng = ((lng2 - lng1) * Math.PI) / 180; // Convert degrees to radians
+    const dLng = ((lng2 - lng1) * Math.PI) / 180; // Convert degrees to radians
 
     // Convert latitude and longitude values to radians
     lat1 = (lat1 * Math.PI) / 180;
     lat2 = (lat2 * Math.PI) / 180;
     lng1 = (lng1 * Math.PI) / 180;
 
-    let bX = Math.cos(lat2) * Math.cos(dLng);
-    let bY = Math.cos(lat2) * Math.sin(dLng);
+    const bX = Math.cos(lat2) * Math.cos(dLng);
+    const bY = Math.cos(lat2) * Math.sin(dLng);
     let lat3 = Math.atan2(
       Math.sin(lat1) + Math.sin(lat2),
       Math.sqrt((Math.cos(lat1) + bX) * (Math.cos(lat1) + bX) + bY * bY)
@@ -201,139 +202,119 @@ const ItemList = () => {
       ? data
       : data.filter((item) => item.tag === selectedTag);
 
-  // async function getDropOffPoint(dropoffPoint_id: any) {
-  //   try {
-  //     const response = await axios.get(
-  //       `${pointsBaseUrl}points/${dropoffPoint_id}/`
-  //     );
-
-  //     if (response.status === 200) {
-  //       console.log("Data fetched successfully:", response.data);
-  //       return response.data[0];
-  //     } else {
-  //       console.error("Request failed with status:", response.status);
-  //       return null;
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred while fetching data:", error);
-  //     return null;
-  //   }
-  // }
+  const renderTable : boolean = windowWidth > 1250;
 
   return (
-    <div>
-      {tags.length !== 0 && (
-        <div className="sm:w-[55vw] overflow-x-auto p-10">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Imagem</th>
-                <th>Tag</th>
-                <th>Ponto de Recolha</th>
-                <th>
-                  <Dropdown items={tags} onSelect={handleSelectTag} />
-                </th>
-              </tr>
-            </thead>
-            <tbody className="text-xl">
-              {filteredData.map((item, index) => (
-                <tr key={index}>
-                  <td>
-                    <div className="flex items-center space-x-3">
-                      <div className="avatar">
-                        <div className="w-12 h-12 mask mask-squircle">
-                          <img
-                            src={item.image}
-                            alt="Avatar Tailwind CSS Component"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="flex items-center space-x-2">
+      <div>
+          {renderTable ? (
+                  <div className="sm:w-[55vw] overflow-x-auto p-10">
+                      <table className="table">
+                          {/* head */}
+                          <thead>
+                          <tr>
+                              <th>Imagem</th>
+                              <th>Tag</th>
+                              <th>Ponto de Recolha</th>
+                              <th>
+                                  <Dropdown items={tags} onSelect={handleSelectTag}/>
+                              </th>
+                          </tr>
+                          </thead>
+                          <tbody className="text-xl">
+                          {filteredData.map((item, index) => (
+                              <tr key={index}>
+                                  <td>
+                                      <div className="flex items-center space-x-3">
+                                          <div className="avatar">
+                                              <div className="w-12 h-12 mask mask-squircle">
+                                                  <img
+                                                      src={item.image}
+                                                      alt="Avatar Tailwind CSS Component"
+                                                  />
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </td>
+                                  <td>
+                                      <div className="flex items-center space-x-2">
                       <span className="badge badge-ghost badge-md">
                         {item.tag}
                       </span>
-                    </div>
-                  </td>
-                  <td>
-                    {dropPoints
-                      .filter((point) => point.id === item.dropoffPoint_id)
-                      .map((filteredPoint) => filteredPoint.name)}
-                  </td>
-                  <td className="flex justify-center items-center">
-                    <button
-                      className="btn btn-ghost border-primary-content"
-                      onClick={() => setSelectedItem(item)}
-                    >
-                      Detalhes
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                                      </div>
+                                  </td>
+                                  <td>
+                                      {dropPoints
+                                          .filter((point) => point.id === item.dropoffPoint_id)
+                                          .map((filteredPoint) => filteredPoint.name)}
+                                  </td>
+                                  <td className="flex justify-center items-center">
+                                      <button
+                                          className="btn btn-ghost border-primary-content"
+                                          onClick={() => setSelectedItem(item)}
+                                      >
+                                          Detalhes
+                                      </button>
+                                  </td>
+                              </tr>
+                          ))}
+                          </tbody>
+                      </table>
+                  </div>
+              )
+              : (
+                  <div> Hello World </div>
+              )
+          }
           <h2 className="cursor-pointer hover:underline">
-            <button
-              className="btn btn-ghost hover:underline hover:bg-transparent"
-              onClick={() =>
-                (
-                  document.getElementById("contacto") as HTMLDialogElement
-                )?.showModal()
-              }
-            >
-              Não encontraste o que perdeste?
-            </button>
+              <button
+                  className="btn btn-ghost hover:underline hover:bg-transparent"
+                  onClick={() =>
+                      (
+                          document.getElementById("contacto") as HTMLDialogElement
+                      )?.showModal()
+                  }
+              >
+                  Não encontraste o que perdeste?
+              </button>
           </h2>
           <dialog id="contacto" className="modal">
-            <div className="modal-box">
-              <h3 className="text-lg font-bold">Tu aí!</h3>
-              <p className="py-4">
-                Se não encontraste aqui o que perdeste, não desesperes! Manda um
-                e-mail para <b>uachadomachado@gmail.com</b> com uma breve
-                descrição do teu item e faremos o nosso melhor para fazê-lo
-                chegar até ti!
-              </p>
-              <div className="modal-action">
-                <form method="dialog">
-                  <button className="btn">Entendido!</button>
-                </form>
+              <div className="modal-box">
+                  <h3 className="text-lg font-bold">Tu aí!</h3>
+                  <p className="py-4">
+                      Se não encontraste aqui o que perdeste, não desesperes! Manda um
+                      e-mail para <b>uachadomachado@gmail.com</b> com uma breve
+                      descrição do teu item e faremos o nosso melhor para fazê-lo
+                      chegar até ti!
+                  </p>
+                  <div className="modal-action">
+                      <form method="dialog">
+                          <button className="btn">Entendido!</button>
+                      </form>
+                  </div>
               </div>
-            </div>
           </dialog>
-        </div>
-      )}
-      {selectedItem &&(
-        <Modal
-          selectedItem={selectedItem}
-          droppoints={dropPoints}
-          onOpenOtherComponent={openMapComponent}
-        />
-      )}
+          {selectedItem && (
+              <Modal
+                  selectedItem={selectedItem}
+                  droppoints={dropPoints}
+                  onOpenOtherComponent={openMapComponent}
+              />
+          )}
 
-      {showMap && isLoaded && !loadError && (
-        // <LocationModal
-        //   // chamar a api para obter a localização do ponto de recolha
-        //   location={locationData}
-        //   userLocation={null}
-        //   onCloseModal={() => setShowMap(false)}
-        //   calculateMidpoint={calculateMidpoint}
-        // />
-        <LocationModal
-          // chamar a api para obter a localização do ponto de recolha
-          location={
-            dropPoints.filter(
-              (point) => point.id === selectedItem?.dropoffPoint_id
-            )[0]
-          }
-          userLocation={userLocation}
-          onCloseModal={() => setShowMap(false)}
-          calculateMidpoint={calculateMidpoint}
-        />
-      )}
-    </div>
+          {showMap && isLoaded && !loadError && (
+              <LocationModal
+                  // chamar a api para obter a localização do ponto de recolha
+                  location={
+                      dropPoints.filter(
+                          (point) => point.id === selectedItem?.dropoffPoint_id
+                      )[0]
+                  }
+                  userLocation={userLocation}
+                  onCloseModal={() => setShowMap(false)}
+                  calculateMidpoint={calculateMidpoint}
+              />
+          )}
+      </div>
   );
 };
 
