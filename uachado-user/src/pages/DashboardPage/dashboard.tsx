3c630786b73c865e675cc73d7a234ece7507ec92
Daniel <danielmartinsferreira@ua.pt>
2023-12-08 19:59:03 +0000

    Format JSX and optimize responsive view
    The code is reformatted for better alignment and readability. Additional functionality was introduced for adjusting the displayed items based on window size, allowing the UI to switch from a table view to a grid view in response to the window width. This creates a better responsive user experience for different screen sizes.

--- uachado-user/src/pages/DashboardPage/dashboard.tsx
+++ uachado-user/src/pages/DashboardPage/dashboard.tsx
@@ -1,60 +1,60 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import Dropdown from '../../components/NewItem/Dropdown/dropdown';
 
 const Dashboard = () => {
 
     const [data, setData] = useState([
 
-        {
-            "image": "https://media.discordapp.net/attachments/852109272262770710/1166749106669113364/image.png",
-            "description": "Carteira preta",
-            "tag": "Carteiras",
-            "dropoffPoint_id": "Cantina de Santiago",
-            "admittedAt": "2021-04-01T00:00:00.000Z",
-            "isVisible": true,
-        },
-        {
-            "image": "https://www.lenovo.com/medias/lenovo-laptop-yoga-slim-series-feature-2-1.png?context=bWFzdGVyfC9lbWVhL2ltYWdlcy98NjkxMzczfGltYWdlL3BuZ3wvZW1lYS9pbWFnZXMvaDgyL2gzZC8xNTg4MTY4MTk5Mzc1OC5wbmd8OWUxZWI4ZTBjZjRhYTNiN2E2YmZlODEyOTAzYjdmOTc4NTE0ZTdiM2IwMGQ0YzI3MzI0NjVkM2I0NTBmY2U5MA",
-            "description": "Notebook ultrafino",
-            "tag": "Portáteis",
-            "dropoffPoint_id": "Reitoria",
-            "admittedAt": "2021-05-01T00:00:00.000Z",
-            "isVisible": true,
-        },
-        {
-            "image": "https://www.tek4life.pt/media/catalog/product/cache/2/image/800x800/85e4522595efc69f496374d01ef2bf13/s/2/s23__lavender_composta_1.png",
-            "description": "Smartphone Samsung",
-            "tag": "Telemóveis",
-            "dropoffPoint_id": "Cantina de Santiago",
-            "admittedAt": "2021-06-01T00:00:00.000Z",
-            "isVisible": true,
-        },
-        {
-            "image": "https://img.pccomponentes.com/articles/1066/10663343/1111-lenovo-tab-m10-hd-2nd-gen-101-3-32gb-gris.jpg",
-            "description": "Tablet Lenovo",
-            "tag": "Tablets",
-            "dropoffPoint_id": "Cantina do Crasto",
-            "admittedAt": "2021-07-01T00:00:00.000Z",
-            "isVisible": true,
-        },
-        {
-            "image": "https://nanochip.pt/wp-content/uploads/Produtos/JBLT520BTAZUL/headphone-jbl-tune-t520-5-3-le-bluetooth-azul-0.jpg",
-            "description": "Auscultadores JBL",
-            "tag": "Auscultadores/Fones",
-            "dropoffPoint_id": "Cantina do Crasto",
-            "admittedAt": "2021-08-01T00:00:00.000Z",
-            "isVisible": true,
-        },
-        {
-            "image": "https://www.worten.pt/i/370d3f3ddc5f01b5fb58963e70730d74e5d61626.jpg",
-            "description": "Carregador portátil universal",
-            "tag": "Carregadores",
-            "dropoffPoint_id": "CP",
-            "admittedAt": "2021-08-01T00:00:00.000Z",
-            "isVisible": true,
-        }
+            {
+                "image": "https://media.discordapp.net/attachments/852109272262770710/1166749106669113364/image.png",
+                "description": "Carteira preta",
+                "tag": "Carteiras",
+                "dropoffPoint_id": "Cantina de Santiago",
+                "admittedAt": "2021-04-01T00:00:00.000Z",
+                "isVisible": true,
+            },
+            {
+                "image": "https://www.lenovo.com/medias/lenovo-laptop-yoga-slim-series-feature-2-1.png?context=bWFzdGVyfC9lbWVhL2ltYWdlcy98NjkxMzczfGltYWdlL3BuZ3wvZW1lYS9pbWFnZXMvaDgyL2gzZC8xNTg4MTY4MTk5Mzc1OC5wbmd8OWUxZWI4ZTBjZjRhYTNiN2E2YmZlODEyOTAzYjdmOTc4NTE0ZTdiM2IwMGQ0YzI3MzI0NjVkM2I0NTBmY2U5MA",
+                "description": "Notebook ultrafino",
+                "tag": "Portáteis",
+                "dropoffPoint_id": "Reitoria",
+                "admittedAt": "2021-05-01T00:00:00.000Z",
+                "isVisible": true,
+            },
+            {
+                "image": "https://www.tek4life.pt/media/catalog/product/cache/2/image/800x800/85e4522595efc69f496374d01ef2bf13/s/2/s23__lavender_composta_1.png",
+                "description": "Smartphone Samsung",
+                "tag": "Telemóveis",
+                "dropoffPoint_id": "Cantina de Santiago",
+                "admittedAt": "2021-06-01T00:00:00.000Z",
+                "isVisible": true,
+            },
+            {
+                "image": "https://img.pccomponentes.com/articles/1066/10663343/1111-lenovo-tab-m10-hd-2nd-gen-101-3-32gb-gris.jpg",
+                "description": "Tablet Lenovo",
+                "tag": "Tablets",
+                "dropoffPoint_id": "Cantina do Crasto",
+                "admittedAt": "2021-07-01T00:00:00.000Z",
+                "isVisible": true,
+            },
+            {
+                "image": "https://nanochip.pt/wp-content/uploads/Produtos/JBLT520BTAZUL/headphone-jbl-tune-t520-5-3-le-bluetooth-azul-0.jpg",
+                "description": "Auscultadores JBL",
+                "tag": "Auscultadores/Fones",
+                "dropoffPoint_id": "Cantina do Crasto",
+                "admittedAt": "2021-08-01T00:00:00.000Z",
+                "isVisible": true,
+            },
+            {
+                "image": "https://www.worten.pt/i/370d3f3ddc5f01b5fb58963e70730d74e5d61626.jpg",
+                "description": "Carregador portátil universal",
+                "tag": "Carregadores",
+                "dropoffPoint_id": "CP",
+                "admittedAt": "2021-08-01T00:00:00.000Z",
+                "isVisible": true,
+            }
 
-    ]
+        ]
     );
 
     const tags = [
@@ -98,9 +98,9 @@ const Dashboard = () => {
         dropoffPoint_id: string;
         admittedAt: string;
         isVisible: boolean;
-     };
-     
-     const [selectedItem, setSelectedItem] = useState<ItemType | null>(null);
+    };
+
+    const [selectedItem, setSelectedItem] = useState<ItemType | null>(null);
     const [filteredData, setFilteredData] = useState(data.filter(item =>
         (selectedTag === "Todos" ? true : item.tag === selectedTag) && item.isVisible
     ));
@@ -109,7 +109,7 @@ const Dashboard = () => {
     const [selectedIndices, setSelectedIndices] = useState(new Set());
 
     // Handle checkbox selection
-    const handleCheckboxChange = (index : any) => {
+    const handleCheckboxChange = (index: number) => {
         setSelectedIndices(prevIndices => {
             const newIndices = new Set(prevIndices);
             if (newIndices.has(index)) {
@@ -136,7 +136,7 @@ const Dashboard = () => {
 
     useEffect(() => {
         if (selectedItem !== null) {
-            const modal: any = document.getElementById("my_modal_1");
+            const modal = document.getElementById("my_modal_1") as HTMLDialogElement;
             modal.showModal();
         }
     }, [selectedItem]);
@@ -146,93 +146,145 @@ const Dashboard = () => {
             (selectedTag === "Todos" ? true : item.tag === selectedTag) && item.isVisible
         ));
     }, [data, selectedTag]);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const renderTable: boolean = windowWidth > 1250;
+
+    const openItemDetails = (item: ItemType) => {
+        setSelectedItem(item);
+        const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
+        modal.showModal();
+    }
 
     // Filter the itemsData based on the selected tag
 
     return (
         <div>
-            <div className="sm:w-[55vw] overflow-x-auto p-10">
-                {/* Button for marking items as found */}
-                <button
-                    className={`btn ${selectedIndices.size > 0 ? 'bg-primary' : 'btn-disabled'}`}
-                    onClick={() => selectedIndices.size > 0 && (document.getElementById('my_modal_5') as HTMLDialogElement)!.showModal()}
-                >
-                    Marcar Itens como Encontrados
-                </button>
+            {renderTable ? (
+                    <div className="sm:w-[55vw] overflow-x-auto p-10">
+                        {/* Button for marking items as found */}
+                        <button
+                            className={`btn ${selectedIndices.size > 0 ? 'bg-primary' : 'btn-disabled'}`}
+                            onClick={() => selectedIndices.size > 0 && (document.getElementById('my_modal_5') as HTMLDialogElement)!.showModal()}
+                        >
+                            Marcar Itens como Encontrados
+                        </button>
 
-                {/* New modal for confirming item removal */}
-                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                    <div className="modal-box">
-                        <h3 className="text-lg font-bold">Marcar itens como encontrados?</h3>
-                        <div className="modal-action">
-                            <form method="dialog">
-                                <button
-                                    className="btn"
-                                    onClick={removeSelectedItems}
-                                >
-                                    Marcar
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </dialog>
-                <h1 className='text-3xl'>Dashboard para: <b>DETI (Sr.Fidalgo)</b></h1>
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>Imagem</th>
-                            <th>Tag</th>
-                            <th>Admitido em</th>
-                            <th>
-                                <Dropdown items={tags} onSelect={handleSelectTag} />
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody className='text-xl'>
-                        {filteredData.map((item, index) => (
-                            console.log(filteredData),
-                            <tr key={index}>
-                                <td>
-                                    <div className="flex items-center space-x-3">
-                                        <div className="avatar">
-                                            <div className="w-12 h-12 mask mask-squircle">
-                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div className="flex items-center space-x-2">
-                                        <span className="badge badge-ghost badge-md">{item.tag}</span>
-                                    </div>
-                                </td>
-                                <td>{new Date(item.admittedAt).toLocaleDateString()}</td>
-                                <td className='items-center'>
-                                    <button
-                                        className="btn btn-ghost border-primary-content"
-                                        onClick={() => setSelectedItem(item)}
-                                    >
-                                        Detalhes
-                                    </button>
-                                </td>
-                                <td>
-                                    <input
-                                        type="checkbox"
-                                        className="checkbox"
-                                        checked={selectedIndices.has(index)}
-                                        onChange={() => handleCheckboxChange(index)}
-                                    />
-                                </td>
+                        {/* New modal for confirming item removal */}
+                        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                            <div className="modal-box">
+                                <h3 className="text-lg font-bold">Marcar itens como encontrados?</h3>
+                                <div className="modal-action">
+                                    <form method="dialog">
+                                        <button
+                                            className="btn"
+                                            onClick={removeSelectedItems}
+                                        >
+                                            Marcar
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </dialog>
+                        <h1 className='text-3xl'>Dashboard para: <b>DETI (Sr.Fidalgo)</b></h1>
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                            <tr>
+                                <th>Imagem</th>
+                                <th>Tag</th>
+                                <th>Admitido em</th>
+                                <th>
+                                    <Dropdown items={tags} onSelect={handleSelectTag}/>
+                                </th>
                             </tr>
+                            </thead>
+                            <tbody className='text-xl'>
+                            {filteredData.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>
+                                            <div className="flex items-center space-x-3">
+                                                <div className="avatar">
+                                                    <div className="w-12 h-12 mask mask-squircle">
+                                                        <img src={item.image} alt="Avatar Tailwind CSS Component"/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="flex items-center space-x-2">
+                                                <span className="badge badge-ghost badge-md">{item.tag}</span>
+                                            </div>
+                                        </td>
+                                        <td>{new Date(item.admittedAt).toLocaleDateString()}</td>
+                                        <td className='items-center'>
+                                            <button
+                                                className="btn btn-ghost border-primary-content"
+                                                onClick={() => setSelectedItem(item)}
+                                            >
+                                                Detalhes
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <input
+                                                type="checkbox"
+                                                className="checkbox"
+                                                checked={selectedIndices.has(index)}
+                                                onChange={() => handleCheckboxChange(index)}
+                                            />
+                                        </td>
+                                    </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ) :
+                (
+                    <div className="grid grid-cols-1 gap-4 m-auto md:grid-cols-2">
+                        <Dropdown items={tags} onSelect={handleSelectTag} className="md:col-span-2"/>
+                        {filteredData.map((item, index) => (
+                            <div
+                                key={index}
+                                className="card bg-secondary-focus"
+                                onClick={() => openItemDetails(item)}
+                            >
+                                <figure className="h-40 overflow-hidden">
+                                    <img
+                                        src={item.image}
+                                        alt={item.description}
+                                        className="object-cover w-full h-full"
+                                    />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title text-3xl sm:text-2xl mx-auto">
+                                        {item.description}
+                                    </h2>
+                                    <p className="text-xs">
+                                        {item.dropoffPoint_id}
+                                    </p>
+                                    <div className="card-actions">
+                                        <button className="btn btn-accent btn-block text-xs sm:text-md">
+                                            Ver Detalhes
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         ))}
-                    </tbody>
-                </table>
-            </div>
+                    </div>
+                )}
             {selectedItem && (
                 <dialog id="my_modal_1" className="modal">
                     <div className="modal-box">
-                        <img src={selectedItem["image"]} alt={selectedItem["description"]} />
+                        <img src={selectedItem["image"]} alt={selectedItem["description"]}/>
                         <h3 className="text-lg font-bold">{selectedItem["description"]}</h3>
                         <span className="badge badge-ghost badge-md">{selectedItem["tag"]}</span>
                         <h1 className="text-xl font-bold">Guardado em: <b>{selectedItem["dropoffPoint_id"]}</b></h1>
