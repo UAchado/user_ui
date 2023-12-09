eb0b003997c7b630831dc0a543f60c4971c76731
Daniel <danielmartinsferreira@ua.pt>
2023-12-09 02:24:15 +0000

    Extract DashboardTable component from DashboardPage
    The previous table view code block within DashboardPage has been abstracted into a separate DashboardTable component for modularity. This new component contains the logic for displaying and manipulating item data, making the parent DashboardPage cleaner and more focused. Additionally, code related to the 'mark as found' feature has been adjusted for better legibility.

--- uachado-user/src/components/DashboardTable/dashboardTable.tsx
+++ uachado-user/src/components/DashboardTable/dashboardTable.tsx
@@ -1,9 +1,22 @@
-import {useState, useEffect} from 'react';
-import {DashboardTable} from '../../components/DashboardTable/dashboardTable';
-import Dropdown from '../../components/NewItem/Dropdown/dropdown';
+import React, {useEffect, useState} from 'react';
+import Dropdown from "../NewItem/Dropdown/dropdown.tsx";
 
-const Dashboard = () => {
 
+export const DashboardTable: React.FC = (props) => {
+
+    const tags = props.tags;
+
+    interface ItemType {
+        image: string;
+        description: string;
+        tag: string;
+        dropoffPoint_id: string;
+        admittedAt: string;
+        isVisible: boolean;
+    }
+
+    const [selectedItem, setSelectedItem] = useState<ItemType | null>(null);
+    const [selectedTag, setSelectedTag] = useState("Todos");
     const [data, setData] = useState([
 
             {
@@ -58,49 +71,6 @@ const Dashboard = () => {
         ]
     );
 
-    const tags = [
-        "Todos",
-        "Portáteis",
-        "Telemóveis",
-        "Tablets",
-        "Auscultadores/Fones",
-        "Carregadores",
-        "Pen drives",
-        "Câmaras",
-        "Livros",
-        "Cadernos",
-        "Material de escritório",
-        "Carteiras",
-        "Chaves",
-        "Cartão",
-        "Óculos",
-        "Joalharia",
-        "Casacos",
-        "Chapéus/Bonés",
-        "Cachecóis",
-        "Luvas",
-        "Mochilas",
-        "Equipamento desportivo",
-        "Garrafas de água",
-        "Guarda-chuvas",
-        "Instrumentos musicais",
-        "Material de arte",
-        "Bagagem",
-        "Produtos de maquilhagem",
-        "Artigos de higiene",
-        "Medicamentos",
-    ];
-    // Rename the state variable to itemsData
-    const [selectedTag, setSelectedTag] = useState("Todos");
-    type ItemType = {
-        image: string;
-        description: string;
-        tag: string;
-        dropoffPoint_id: string;
-        admittedAt: string;
-        isVisible: boolean;
-    };
-    const [selectedItem, setSelectedItem] = useState<ItemType | null>(null);
     const [filteredData, setFilteredData] = useState<ItemType[]>(data);
     const handleSelect = (item: ItemType) => {
         // Update the visibility of item directly
@@ -114,6 +84,11 @@ const Dashboard = () => {
     const handleSelectTag = (tag: string) => {
         setSelectedTag(tag);
     };
+    const openItemDetails = (item: ItemType) => {
+        setSelectedItem(item);
+        const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
+        modal.showModal();
+    }
     useEffect(() => {
         setFilteredData(data.filter(item =>
             (selectedTag === "Todos" ? true : item.tag === selectedTag) && item.isVisible
@@ -126,70 +101,61 @@ const Dashboard = () => {
             modal.showModal();
         }
     }, [selectedItem]);
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
 
-    const renderTable: boolean = windowWidth > 1250;
-
-    const openItemDetails = (item: ItemType) => {
-        setSelectedItem(item);
-        const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
-        modal.showModal();
-    }
     return (
-        <div>
-            {renderTable ? (
-                <DashboardTable data={data} setData={setData} setSelectedItem={setSelectedItem}/>
-
-                ) :
-                (
-                    <div className="grid grid-cols-1 gap-4 m-10 md:grid-cols-2">
-                        <Dropdown items={tags} onSelect={handleSelectTag} className="md:col-span-2"/>
-                        {filteredData.map((item, index) => (
-                            <div
-                                key={index}
-                                className="card bg-secondary-focus"
-                            >
-                                <figure className="h-40 overflow-hidden">
-                                    <img
-                                        src={item.image}
-                                        alt={item.description}
-                                        className="object-cover w-full h-full"
-                                    />
-                                </figure>
-                                <div className="card-body">
-                                    <h2 className="card-title text-3xl sm:text-2xl mx-auto">
-                                        {item.description}
-                                    </h2>
-                                    <p className="text-xs">
-                                        {item.dropoffPoint_id}
-                                    </p>
-                                    <div className="card-actions">
-                                        <button className="btn btn-accent btn-block text-xs sm:text-md"
-                                                onClick={() => openItemDetails(item)}
-                                        >
-                                            Ver Detalhes
-                                        </button>
-                                        <button
-                                            className="btn btn-neutral btn-block"
-                                            onClick={() => handleSelect(item)}
-                                        >
-                                            Marcar como encontrado
-                                        </button>
+        <div className="sm:w-[55vw] overflow-x-auto p-10">
+            <h1 className='text-3xl'>Dashboard para: <b>DETI (Sr.Fidalgo)</b></h1>
+            <table className="table">
+                {/* head */}
+                <thead>
+                <tr>
+                    <th>Imagem</th>
+                    <th>Tag</th>
+                    <th>Admitido em</th>
+                    <th></th>
+                    <th>
+                        <Dropdown items={tags} onSelect={handleSelectTag}/>
+                    </th>
+                </tr>
+                </thead>
+                <tbody className='text-xl'>
+                {filteredData.map((item, index) => (
+                    <tr key={index}>
+                        <td>
+                            <div className="flex items-center space-x-3">
+                                <div className="avatar">
+                                    <div className="w-12 h-12 mask mask-squircle">
+                                        <img src={item.image} alt="Avatar Tailwind CSS Component"/>
                                     </div>
                                 </div>
                             </div>
-                        ))}
-                    </div>
-                )}
+                        </td>
+                        <td>
+                            <div className="flex items-center space-x-2">
+                                <span className="badge badge-ghost badge-md">{item.tag}</span>
+                            </div>
+                        </td>
+                        <td>{new Date(item.admittedAt).toLocaleDateString()}</td>
+                        <td>
+                            <button
+                                className="btn btn-ghost border-primary-content"
+                                onClick={() => openItemDetails(item)}
+                            >
+                                Detalhes
+                            </button>
+                        </td>
+                        <td>
+                            <button
+                                className="btn btn-neutral"
+                                onClick={() => handleSelect(item)}
+                            >
+                                Marcar como encontrado
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
             {selectedItem && (
                 <dialog id="my_modal_1" className="modal">
                     <div className="modal-box">
@@ -210,9 +176,8 @@ const Dashboard = () => {
                     </div>
                 </dialog>
             )}
-
         </div>
-    );
-};
 
-export default Dashboard;
+
+    );
+}
